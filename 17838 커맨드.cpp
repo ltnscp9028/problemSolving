25a35102335095f8c4c7e8da8d0a13f17e045a76
y0on2q <ltnscp9028@gmail.com>
2020-06-20 14:12:57 +0900

    17838 커맨드

---
+++ 17838 커맨드.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t;
+string s;
+int main(){
+	for(cin>>t;t--;
+	puts(s.length()==7&&
+	s[0]==s[1]&&s[1]^s[2]&&s[2]==s[3]&&s[3]^s[4]&&s[4]^s[5]&&s[5]==s[6]?"1":"0"))
+		cin>>s;
+}
