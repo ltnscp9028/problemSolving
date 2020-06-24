484587c036d8d345efbb9137329923644b8217f1
y0on2q <ltnscp9028@gmail.com>
2020-06-24 10:49:24 +0900

    2019-5 비즈니스 프로그래밍

--- 2019-5.cpp
+++ 2019-5.cpp
@@ -3,16 +3,16 @@ using namespace std;
 stack<int>q;
 vector<pair<int,int>>v;
 string s;
-int len,i,cnt;
+int len,i,cntr,cntl;
 int main(){
 	cin>>s;
 	len = s.length();
 	if(len<3||len>30)return puts("입력오류");
 	for(;i<len;i++){ 
-		if(s[i]=='(')cnt++;
-		if(s[i]==')')cnt--;
+		if(s[i]=='(')cntr++;
+		if(s[i]==')')cntl++;
 	}
-	if(cnt)return puts("수식오류");
+	if(cntr!=cntl || cntr==0)return puts("수식오류");
 	for(i=0;i<len;i++){
 		if(s[i]=='(')q.push(i);
 		if(s[i]==')'){
