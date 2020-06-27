1b2846d9d3bf4b1dcb91783eade22411dd067168
y0on2q <ltnscp9028@gmail.com>
2020-06-27 17:17:42 +0900

    short

--- 17838 커맨드.cpp
+++ 17838 커맨드.cpp
@@ -1,8 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-int t;
 string s;
-int main(){
+int main(int t){
 	for(cin>>t;t--;
 	puts(s.length()==7&&
 	s[0]==s[1]&&s[1]^s[2]&&s[2]==s[3]&&s[3]^s[4]&&s[4]^s[5]&&s[5]==s[6]?"1":"0"))
