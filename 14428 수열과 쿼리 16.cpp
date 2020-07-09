15e77fc05c07e4ec04b177c97b2a56460658ca77
y0on2q <ltnscp9028@gmail.com>
2020-07-09 11:36:42 +0900

    14428 수열과 쿼리 16

---
+++ 14428 수열과 쿼리 16.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[100001],tree[400004],b;
+int n,q,x,y,a;
+
+int idx_query(int dx,int dy){
+	return dx==-1?dy:dy==-1?dx:arr[dx]==arr[dy]?dx<dy?dx:dy:arr[dx]<=arr[dy]?dx:dy;
+}
+
+int init(int node,int st,int ed){
+	if(st==ed)return tree[node]=st;
+	int mid = (st+ed)/2;
+	return tree[node] = idx_query(init(node*2,st,mid), init(node*2+1,mid+1,ed));
+}
+
+int update(int node,int st,int ed, int i){
+	if(i<st||ed<i)return tree[node];
+	if(st==ed)return tree[node];
+	int mid = (st+ed)/2;
+	return tree[node] = idx_query(update(node*2,st,mid,i),update(node*2+1,mid+1,ed,i));
+}
+
+int query(int node,int st,int ed,int i, int j){
+	if(j<st || ed<i)return -1;
+	if(i<=st&&ed<=j)return tree[node];
+	int mid = (st+ed)/2;
+	return idx_query(query(node*2,st,mid,i,j),query(node*2+1,mid+1,ed,i,j));
+}
+
+int main(){
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)scanf("%d",&arr[i]);
+	init(1,0,n-1);
+	for(scanf("%d",&q);q--;){
+		scanf("%d %d %d",&a,&x,&y);
+		if(a==1){
+			arr[x-1]=y;
+			update(1,0,n-1,x-1);
+		}
+		else printf("%d\n",query(1,0,n-1,x-1,y-1)+1);
+	}
+}
