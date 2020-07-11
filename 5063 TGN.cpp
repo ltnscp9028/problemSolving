93981c6fa03a93fcf64e621f73eda49002da51f8
y0on2q <ltnscp9028@gmail.com>
2020-07-11 14:23:28 +0900

    5063 TGN

--- 5063 TGN.cpp
+++ 5063 TGN.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int a,r,b;
 int main(){
-	for(cin>>a;~scanf("%d %d %d",&a,&b,&r);)puts(b-r>a?"advertise":b-r<a?"do not advertise":"does not matter");
+	for(cin>>a;~scanf("%d %d %d",&a,&b,&r);)
+		puts(b-r>a?"advertise":b-r<a?"do not advertise":"does not matter");
 }
