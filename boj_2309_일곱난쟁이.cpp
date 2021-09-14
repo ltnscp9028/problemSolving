a3f68c50b2f0891c07f437935e3e75d5fd35d162
y0on2q <ltnscp9028@gmail.com>
2021-09-14 20:37:18 +0900

    Fix:boj 2309

--- boj_2309_일곱난쟁이.cpp
+++ boj_2309_일곱난쟁이.cpp
@@ -2,6 +2,6 @@
 int a[9],j,i,s=-100,p,q;main(){
   for(;~scanf("%d",&a[i]);s+=a[i++]);
   for(std::sort(a,a+9);j<9;j++)
-    for(i=0;i<9;a[j]+a[i]^s?0:(p=j,q=i),i++);
+    for(i=-1;++i<9;a[j]+a[i]^s?0:(p=j,q=i));
   for(i=-1;++i<9;i^p&&i^q?printf("%d ",a[i]):0);
 }
