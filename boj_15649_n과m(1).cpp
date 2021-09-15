39a3d32b3fd5a4d0bc8e769de8c8ccc576c89f67
y0on2q <ltnscp9028@gmail.com>
2021-09-15 21:32:10 +0900

    Add: boj_15649_n과m(1)

---
+++ boj_15649_n과m(1).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+int n,m,a[8],v[8];
+void f(int c){
+  if(c==m){
+    for(int i=0;i<m;)printf("%d ",a[i++]);
+    puts("");
+  }
+  for(int i=0;i<n;i++)
+  if(!v[i]){
+    v[i]=1;
+    a[c]=i+1;
+    f(c+1);
+    v[i]=0;
+    }
+  }
+main(){
+  std::cin>>n>>m;
+  f(0);
+}
