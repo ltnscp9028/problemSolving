c06d7a828500c3515b847b0cab12cc9cebb9f6bd
y0on2q <ltnscp9028@gmail.com>
2021-09-24 00:03:15 +0900

    Fix:1707

--- boj_1707_이분그래프.cpp
+++ boj_1707_이분그래프.cpp
@@ -17,47 +17,18 @@ int dfs(int n, int g) {
     return 1;
 }
 
- main() {
-    int k, v, e;
-    scanf("%d", &k);
-    while (k--) {
-        scanf("%d %d", &v, &e);
-        for (int i=1; i<=v; i++) {
-            // Initialize
-            a[i].clear();
-            check[i] = 0;
-        }
-        int x, y;
-        for (int i=0; i<e; i++) {
-            scanf("%d %d", &x, &y);
-            a[x].push_back(y);
-            a[y].push_back(x);
-        }
-        bool ans = true;
-        for (int i=1; i<=v; i++) {
-            if (check[i] == 0) {
-                if (!dfs(i, 1)) {
-                    ans = false;
-                    break;
-                }
-            }
-        }
-        printf("%s\n", ans ? "YES" : "NO");
-    }
-    return 0;
-}
 
-//main() {
-//    for(cin>>k;k--;){
-//    	cin>>v>>e;
-//    	for(i=0;++i<=v;check[i]=0)a[i].clear();
-//    	for(i=0;i<e;a[x].push_back(y),a[y].push_back(x),i++)cin>>x>>y;
-//    	for(i=0;++i<=v;)
-//    		if(!check[i])
-//    			if(!dfs(i,1)){
-//    				ans=0;
-//    				break;
-//				}
-//		cout<<ans? "YES":"NO"<<'\n';
-//	}
-//}
+main() {
+    for(cin>>k;k--;){
+    	cin>>v>>e;
+    	for(i=0;++i<=v;check[i]=0)a[i].clear();
+    	for(i=0;i<e;a[x].push_back(y),a[y].push_back(x),i++)cin>>x>>y;
+    	for(ans=i=1;i<=v;i++)
+    		if(!check[i])
+    			if(!dfs(i,1)){
+    				ans=0;
+    				break;
+				}
+		puts(ans?"YES":"NO");
+	}
+}
