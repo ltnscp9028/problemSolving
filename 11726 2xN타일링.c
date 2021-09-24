66bd5a42f380444f25209e6ac60c31b8503e0515
y0on2q <ltnscp9028@gmail.com>
2021-09-24 23:41:58 +0900

    Fix:11727

--- 11726 2xN타일링.c
+++ 11726 2xN타일링.c
@@ -1,17 +1,4 @@
-dp[1001]={1,1};n;b=10007;
-
-main(i){
-	for(scanf("%d",&n);++i<=n;dp[i]=(dp[i-1]+dp[i-2])%b);
-	printf("%d",dp[n]%b);
-}
-
-
-
-/*f,a;main(r){
-	for(scanf("%d",&a);a--;r%=10007){
-		r+=f,f=r-f;
-		printf("r=%d f=%d\n",r,f);
-		}
+f,a;main(r){
+	for(scanf("%d",&a);a--;r%=10007)r+=f,f=(r-f)*2;
 	printf("%d",r);
 }
-*/
