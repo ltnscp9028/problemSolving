999e61c27822bbeb15e1303362317db8b00f37b7
y0on2q <ltnscp9028@gmail.com>
2021-09-27 23:26:44 +0900

    Add: boj_6588_골드바흐의추측

---
+++ boj_6588_골드바흐의추측.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+const int m=1e6;
+int arr[m+9],n,i,j,l;
+main(){
+	for(i=2;i*i<m;i++)
+		if(!arr[i])
+			for(j=i*i;j<m;j+=i)
+				arr[j]=1;
+
+	for(;~scanf("%d",&n);){
+		for(i=2;++i<n;)
+			if(!arr[i]&&!arr[n-i]){
+				printf("%d = %d + %d\n",n,i,n-i);
+				break;
+			}	
+	}
+}
