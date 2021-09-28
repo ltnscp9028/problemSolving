49a928f65d35c89d365d8c8d750eea1b5855e82a
y0on2q <ltnscp9028@gmail.com>
2021-09-28 21:03:01 +0900

    Add: boj_10972_다음순열

---
+++ boj_10973_이전순열.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>v;
+int n,i,t;
+main(){
+	for(cin>>n,v.resize(n);i<n;)cin>>v[i++];
+	t = prev_permutation(v.begin(),v.end());
+	for(i=0,t?0:puts("-1");t&&i<n;i++)cout<<v[i]<<' ';
+}
