a4bed334093b67205e08519cd17e4671ced27643
y0on2q <ltnscp9028@gmail.com>
2021-11-01 19:07:40 +0900

    Fix:boj_1107

--- boj_1107_리모컨.cpp
+++ boj_1107_리모컨.cpp
@@ -1,30 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[11],i,p,q,r,ans,c,ln,pr;
-int check(int n) {
+int arr[11],i,p,q,r,ans,c,ln,cln,pr;
+int check(int n){
     if (!n)return !arr[0];
-    int len = 0;
-    while (n>0){
-        if (arr[n % 10]) return 0;
-        n /= 10;
-        len++;
-    }
-    return len;
+    for(;n>0;n/=10,cln++)if(arr[n%10])return 0;
+    return cln;
 }
 main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    for (cin>>p>>q;i<q;arr[r]=1,i++)cin>>r;
-    for (ans = abs(p-100),i=0;i<10000001;i++){
-        c=i;
-        ln=check(c);
-        if (ln>0){
-            pr=abs(c-p);
-            if(ans>pr+ln){
-                ans=pr+ln;
-            }
-        }
-    }
-    cout << ans;
+    for(cin>>p>>q;i<q;arr[r]=1,i++)cin>>r;
+    for(ans=abs(p-100),i=0;i<10000001;cln=0,ln=check(c),ln>0?pr=abs(c-p),ans=ans>pr+ln?pr+ln:ans:0,i++)c=i;
+    cout<<ans;
 }
